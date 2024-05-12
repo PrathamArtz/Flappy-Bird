fece6be7170eb9bb4b761ea770c19a335f20b511
Prathamesh-Pulkanthwar <Pulkanthwarprathamesh2@gmail.com>
2024-05-12 12:07:10 +0530

    pipes move left

--- Assets/Scripts/MovePipes.cs
+++ Assets/Scripts/MovePipes.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class MovePipes : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private float _pipeSpeed = 3.0f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector2.left * _pipeSpeed * Time.deltaTime);
     }
 }
